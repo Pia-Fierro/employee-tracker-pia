dfe731d511497bc388eb1fe5ec43dcf53997dfab
pia fierro <piafierro.3187@gmail.com>
2023-02-11 14:42:31 +1000

    Added viewAllEmp(). Missing manager name.

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ db.connect ((err) => {
 function viewallDep () {
     db.connect (function(err) {
         if (err) throw err;
-        db.query("SELECT * FROM department", function (err, result){
+        db.query("SELECT department.department_name AS 'Department name', department.id AS 'Department ID' FROM department", function (err, result){
             if(err) throw err;
             console.log("\n");
             console.table(result);
@@ -91,7 +91,7 @@ function viewallDep () {
 function viewAllRoles () {
     db.connect (function(err) {
         if (err) throw err;
-        db.query("SELECT role.id, role.role_title, role.salary, department.department_name FROM role JOIN department ON role.id = department.id", function (err, result){
+        db.query("SELECT  role.role_title AS 'Job title', role.id AS 'Role ID', role.salary, department.department_name AS 'Department' FROM role JOIN department ON role.id = department.id", function (err, result){
             if(err) throw err;
             console.log("\n");
             console.table(result);
@@ -100,15 +100,14 @@ function viewAllRoles () {
     })
 }
 
-// function viewallEmpl () {
-//     db.connect (function(err) {
-//         if (err) throw err;
-//         db.query("SELECT * FROM employee", function (err, result){
-//             if(err) throw err;
-//             console.log("\n");
-//             console.table(result);
-//             promptUser();
-//         });
-//     })
-// }
-
+function viewallEmpl () {
+    db.connect (function(err) {
+        if (err) throw err;
+        db.query("SELECT employee.id AS 'Employee ID', employee.first_name AS 'Name', employee.last_name AS 'Last name', role.role_title AS 'Job title', role.salary, department.department_name AS 'Department',employee.manager_id FROM employee, role, department WHERE department.id = role.department_id AND role.id = employee.role_id", function (err, result){
+            if(err) throw err;
+            console.log("\n");
+            console.table(result);
+            promptUser();
+        });
+    })
+}
